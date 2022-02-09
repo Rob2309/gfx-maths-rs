3da9cb8e5ee6b05f0c5aab41cbd76948a13176a2
Robin Quint <rob2309@hotmail.de>
2022-02-09 08:31:32 +0100

    Fixed compile errors on stable compiler

--- src/macros.rs
+++ src/macros.rs
@@ -15,7 +15,7 @@ macro_rules! swizzle_type {
 macro_rules! swizzle {
     ($($members:ident),+) => {
         paste::paste! {
-            #[doc(cfg(feature="swizzle"))] // make docs.rs more readable by showing the required feature
+            #[cfg_attr(doc, doc(cfg(feature="swizzle")))] // make docs.rs more readable by showing the required feature
             pub fn [<$($members)+>](&self) -> swizzle_type!($($members),+) {
                 <swizzle_type!($($members),+)>::new(
                     $(self.$members),+
